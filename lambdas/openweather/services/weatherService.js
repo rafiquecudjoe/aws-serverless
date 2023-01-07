dfa7b7e1e5052bd1d01318143ed4d9de0b3edaf8
Rafique Adam Cudjoe <rafiqueacudjoe@gmail.com>
2023-01-07 13:44:01 +0000

    minor update

--- lambdas/openweather/services/weatherService.js
+++ lambdas/openweather/services/weatherService.js
@@ -1,32 +1,32 @@
 
 const axios = require('axios')
 const SecretsManager = require('../utils/secretsManager');
-// const redis = require("redis")
+const redis = require("redis")
 
 const retrieveWeather = async (city) => {
 
-    //     const redisOptions = {
-    //     host: "mycluster.kneaye.0001.use1.cache.amazonaws.com",
-    //     port: 6379
-    // }
+    const redisOptions = {
+        host: "weather-001.yo10re.0001.use1.cache.amazonaws.com",
+        port: 6379
+    }
 
-    // const client = redis.createClient(redisOptions)
+    const client = redis.createClient(redisOptions)
 
-    // console.info('Connected to Redis Server')
-    // client.on('error', (err) => console.log('Redis Client Error: ', err));
+    console.info('Connected to Redis Server')
+    client.on('error', (err) => console.log('Redis Client Error: ', err));
 
-    // console.log("Connecting to Redis...");
-    // await client.connect();
-    // console.log("Connected to Redis");
+    console.log("Connecting to Redis...");
+    await client.connect();
+    console.log("Connected to Redis");
 
-    // const redisKey = city
-    // console.log("Looking up metadata in Redis for " + redisKey);
+    const redisKey = city
+    console.log("Looking up metadata in Redis for " + redisKey);
 
-    // const redisMeta = await client.get(redisKey);
+    const redisMeta = await client.get(redisKey);
 
-    // if (redisMeta){
-    //    return res.status(200).json({ status:true, data: redisMeta});
-    // }
+    if (redisMeta) {
+        return res.status(200).json({ status: true, data: redisMeta });
+    } 
 
     var secretName = "weather";
     var region = "us-east-1";
@@ -40,7 +40,13 @@ const retrieveWeather = async (city) => {
 
     const currenWeather = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${openApiKey}`)
 
+    await client.set(city, currenWeather.data)
+    
+    await client.disconnect()
+
     return currenWeather.data
+
+    // const celCius = 500 - 273.15
 
     // // STORE TO REDIS
     // await client.set(redisKey, JSON.stringify(CurrenWeather.data));
